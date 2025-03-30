146cb78418634880cdd9f9318761c5d015baff41
Archit <archittyagi599@gmail.com>
2025-03-30 20:45:59 +0530

    Create day_1.cpp

---
+++ day_1.cpp
@@ -0,0 +1,10 @@
+// Today (day 1) I spend my maximum time to setup C++ compiler in Ubuntu also with VS Code.
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello World";
+
+    return 0;
+}
