d5f3a6dba06ed035ac7a2fb253d0cb7881ed0439
Archit <archittyagi599@gmail.com>
2025-04-01 04:00:20 +0530

    day_2

---
+++ day_2.cpp
@@ -0,0 +1,13 @@
+// Today I learn the concept of varibles and datatypes in C++
+
+# include<iostream>
+
+int main()
+{
+    // exploring datatypes
+    int a = 5;
+
+    cout << a;
+
+    return;
+}
