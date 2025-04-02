ec6a124dc5e6077560dd161f59c752e88afeaa2f
Archit <archittyagi599@gmail.com>
2025-04-03 04:19:11 +0530

    Create day_4.cpp

---
+++ day_4.cpp
@@ -0,0 +1,54 @@
+/*
+Today day 4 I study these topics:
+  - String functions
+  - string user input methods
+  - C-Style string
+  - Exploring math library and its methods
+  - Exploring Boolean expression
+*/
+
+# include <iostream>
+# include <cmath>
+
+using namespace std;
+
+int main(){
+
+    // string s = "hello I am a world";
+    // s.at(s.length()-1) = 'D';
+    // // cout << s;
+
+    // // user input string
+    // string name;
+    // cout << "Enter your name: ";
+    // // cin >> name;
+    // getline(cin, name);
+    // cout << "Welcome " << name;
+
+
+    // // C-Style string
+    // char name[] = "rahul";
+    // cout << name;
+
+
+    // // ----------------- Exploring math library and its functions --------------------- //
+    // int a=10, b=20;
+
+    // cout << max(a,b) << endl;
+    // cout << min(a,b) << endl;
+    // cout << sqrt(b) << endl;
+    // cout << log(a) << endl;
+    // cout << round(a/b) << endl;
+
+
+
+    // -------------------- Exploring boolean expression ------------------------------- //
+    int a=0, b=10;
+    cout << (a>b);
+
+
+
+    cout << "\n\n";
+    return 0;
+}
+
