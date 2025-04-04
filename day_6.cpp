0a132abb06ac6805656726aa779a62c1df234166
Archit <archittyagi599@gmail.com>
2025-04-05 04:40:12 +0530

    Create day_6.cpp

---
+++ day_6.cpp
@@ -0,0 +1,19 @@
+// day 6
+
+// Today I explore arrays
+
+# include <iostream>
+using namespace std;
+
+int main(){
+
+    string students[] = {"anuj", "kartik", "ayush"};
+
+    cout << students[0] << endl;
+    cout << sizeof(students)/sizeof(students[0]) << endl;
+
+    cout << endl;
+    return 0;
+}
+
+
