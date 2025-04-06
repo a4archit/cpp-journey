18bfbe42ad80f6729b39ad7044753f347da1e9b1
Archit <archittyagi599@gmail.com>
2025-04-06 19:49:16 +0530

    Create day_7.cpp

---
+++ day_7.cpp
@@ -0,0 +1,16 @@
+// day 7
+// today I practice little code
+
+
+# include <iostream>
+
+int main(){
+
+  for (int i=0; i<10; i++){
+    cout << "iter " << to_string(i+1) << endl;
+  }
+
+  cout << endl;
+  return 0;
+}
+
